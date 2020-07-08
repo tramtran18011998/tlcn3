a622d5b85fa62d89d43bf078480bb90ade18e293
Tran Thi Ngoc Tram <tramttn.saiki@gmail.com>
2020-07-08 16:09:58 +0700

    copy files for management-user-service
    - run =>error 401:  Unauthorized
    - still not test login ... (authentication)

--- management-user-service/src/main/java/com/example/demo/DemoApplication.java
+++ management-user-service/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigurationProperties(AppProperties.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
